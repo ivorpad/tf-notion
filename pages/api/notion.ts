63a76e491e75c1d9787c994e0f288a26cd293cd4
ivorpad <ivorjpc@gmail.com>
2022-03-24 13:25:22 +0100

    now id and secret are passed in qs

--- pages/api/notion.ts
+++ pages/api/notion.ts
@@ -23,29 +23,38 @@ function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   await runMiddleware(req, res, cors);
 
-  const data = await fetch(
-    `https://api.notion.com/v1/databases/739f5db81745411fae5ae8f7c9763891/query`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.NOTION_API_KEY}`,
-        "Notion-Version": "2022-02-22"
-      },
-    }
-  ).then(res => res.json());
+  const { id, secret } = req.query;
+
+  if (!secret && !process.env.NOTION_API_KEY) {
+    res.status(401).json({ error: "No secret provided" });
+    return;
+  }
+
+  const data = await fetch(`https://api.notion.com/v1/databases/${id}/query`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${secret ? secret : process.env.NOTION_API_KEY}`,
+      "Notion-Version": "2022-02-22",
+    },
+  }).then((res) => res.json());
+
+  if (data?.object === "error") {
+    res.status(401).json({ error: data.message });
+    return;
+  }
 
   const database = data.results
     .filter((d: any) => d.properties.Content.title.length > 0)
     .map((d: any) => ({
       title: d.properties.Title.rich_text[0].plain_text,
-      content: d.properties.Content.title.map((t: any) => t.plain_text).join(" "),
+      content: d.properties.Content.title
+        .map((t: any) => t.plain_text)
+        .join(" "),
       category: d.properties.Category.multi_select[0]?.name,
     }));
 
-
   return res.json({ response: database });
-}
+};
 
-export default handler  
-
+export default handler;
